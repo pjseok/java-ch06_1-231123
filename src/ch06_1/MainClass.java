9c597ca70e93725da6c875e52a2e1148c28d4a26
pjseok <pjs7850@naver.com>
2023-11-23 15:50:05 +0900

    자바 챕터 6-1 클래스

---
+++ src/ch06_1/MainClass.java
@@ -0,0 +1,22 @@
+package ch06_1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student stu1 = new Student(); //Student 클래스로 객체가 선언
+		
+		stu1.name = "홍길동";
+		stu1.age = 10;
+		
+		Student stu2 = new Student(); //Student 클래스로 객체가 선언
+		
+		stu2.name = "김유신";
+		stu2.age = 20;
+		
+		
+		
+	}
+
+}
---
+++ src/ch06_1/Student.java
@@ -0,0 +1,12 @@
+package ch06_1;
+
+public class Student {
+	
+	// 필드, 속성(값), 멤버변수, 
+	String name;
+	int age;
+	int grade;
+	String phonrNum;
+	
+
+}
